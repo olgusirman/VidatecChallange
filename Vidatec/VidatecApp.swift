b847c3bc0df943d65a396e302e683305ab742c0c
Olgu SIRMAN <olgusirman@gmail.com>
2021-01-09 21:59:56 +0000

    Initial Commit

---
+++ Vidatec/VidatecApp.swift
@@ -0,0 +1,20 @@
+//
+//  VidatecApp.swift
+//  Vidatec
+//
+//  Created by Olgu SIRMAN on 09/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct VidatecApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
