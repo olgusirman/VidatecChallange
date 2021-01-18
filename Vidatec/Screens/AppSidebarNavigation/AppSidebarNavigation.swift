055e8641e03d4b78f55f8ba1fcab6bcdec89204b
Olgu SIRMAN <olgusirman@gmail.com>
2021-01-18 21:50:41 +0000

    sidebar navigation handled

--- Vidatec/Screens/AppSidebarNavigation/AppSidebarNavigation.swift
+++ Vidatec/Screens/AppSidebarNavigation/AppSidebarNavigation.swift
@@ -9,15 +9,43 @@ import SwiftUI
 
 struct AppSidebarNavigation: View {
     
+    @State private var selection: NavigationItem? = .people
+    
+    enum NavigationItem {
+        case people
+        case rooms
+    }
+    
+    var sidebar: some View {
+        List(selection: $selection) {
+            NavigationLink(destination: PeopleView(), tag: NavigationItem.people, selection: $selection) {
+                Label("People", systemImage: "list.bullet")
+            }
+            .tag(NavigationItem.people)
+            
+            NavigationLink(destination: RoomsView(), tag: NavigationItem.rooms, selection: $selection) {
+                Label("Rooms", systemImage: "heart")
+            }
+            .tag(NavigationItem.rooms)
+        }
+        .listStyle(SidebarListStyle())
+    }
+    
     var body: some View {
-        VStack {
-            Text("Select a category")
-                .foregroundColor(.secondary)
-            Text("Select a smoothie")
-                .foregroundColor(.secondary)
+        NavigationView {
+            sidebar
         }
     }
+    
 }
+
+extension AppSidebarNavigation {
+    enum Tab {
+        case people
+        case rooms
+    }
+}
+
 
 struct AppSidebarNavigation_Previews: PreviewProvider {
     static var previews: some View {
